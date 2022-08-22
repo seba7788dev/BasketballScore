e87296b3be36eee64eee8bf477b9f8dc9684c9cc
seba7788dev <seby77_88@hotmail.com>
2022-08-22 01:48:37 -0300

    correccion de detalles en activity pcipal y agregacion de funciones

--- app/src/main/java/com/example/basketballscore/MainActivity.kt
+++ app/src/main/java/com/example/basketballscore/MainActivity.kt
@@ -8,14 +8,15 @@ import android.widget.TextView
 import com.example.basketballscore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    var localPuntos:Int=0
+    var visitantePuntos:Int=0
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding= ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        var localPuntos:Int=0
-        var visitantePuntos:Int=0
+
 
         binding.localPuntos.text=localPuntos.toString()
         binding.visitantePuntos.text=visitantePuntos.toString()
@@ -49,23 +50,22 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.localMasUno.setOnClickListener {
-            localPuntos+=1
-            binding.localPuntos.text=localPuntos.toString()
+            sumarUnPuntoLocal(binding.localPuntos)
         }
 
         binding.localMasDos.setOnClickListener {
-            localPuntos+=2
-            binding.localPuntos.text=localPuntos.toString()
+            sumarDosPuntosLocal(binding.localPuntos)
         }
 
         binding.visitanteMasUno.setOnClickListener {
-            visitantePuntos+=1
-            binding.visitantePuntos.text=visitantePuntos.toString()
+
+            sumarUnPuntoVisita(binding.visitantePuntos)
+
         }
 
         binding.visitanteMasDos.setOnClickListener {
-            visitantePuntos+=2
-            binding.visitantePuntos.text=visitantePuntos.toString()
+
+            sumarDosPuntosVisita(binding.visitantePuntos)
         }
 
         binding.btnSiguiente.setOnClickListener {
@@ -73,6 +73,124 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+    fun sumarUnPuntoLocal(textView: TextView){
+        localPuntos+=1
+        textView.text=localPuntos.toString()
+
+    }
+    fun sumarDosPuntosLocal(textView: TextView){
+        localPuntos+=2
+        textView.text=localPuntos.toString()
+
+    }
+
+    fun sumarUnPuntoVisita(textView: TextView){
+        visitantePuntos+=1
+        textView.text=visitantePuntos.toString()
+
+    }
+
+    fun sumarDosPuntosVisita(textView: TextView){
+        visitantePuntos+=2
+        textView.text=visitantePuntos.toString()
+
+    }
+
+
+
+    //funca para el orto
+    fun sumarPuntos(textView: TextView,cantPuntos:Int, localOVisita:Int){
+        var cantidadPuntos=cantPuntos
+        var localia=localOVisita
+
+        localia+=cantidadPuntos
+        textView.text=localia.toString()
+    }
+
+
 
 
 }
+
+//***********************************************
+
+
+/**
+
+class MainActivity : AppCompatActivity() {
+var localPuntos:Int=0
+var visitantePuntos:Int=0
+private lateinit var binding: ActivityMainBinding
+
+override fun onCreate(savedInstanceState: Bundle?) {
+binding= ActivityMainBinding.inflate(layoutInflater)
+super.onCreate(savedInstanceState)
+setContentView(binding.root)
+
+
+binding.localPuntos.text=localPuntos.toString()
+binding.visitantePuntos.text=visitantePuntos.toString()
+
+
+binding.botonRestart.setOnClickListener {
+localPuntos=0
+visitantePuntos=0
+binding.localPuntos.text=localPuntos.toString()
+binding.visitantePuntos.text=visitantePuntos.toString()
+}
+
+binding.localMenos.setOnClickListener {
+if (binding.localPuntos.text!="0"){
+localPuntos-=1
+binding.localPuntos.text=localPuntos.toString()
+
+}
+
+
+}
+
+binding.visitanteMenos.setOnClickListener {
+if (binding.visitantePuntos.text!="0"){
+visitantePuntos-=1
+binding.visitantePuntos.text=visitantePuntos.toString()
+
+}
+
+
+}
+
+binding.localMasUno.setOnClickListener {
+localPuntos+=1
+binding.localPuntos.text=localPuntos.toString()
+}
+
+binding.localMasDos.setOnClickListener {
+localPuntos+=2
+binding.localPuntos.text=localPuntos.toString()
+}
+
+binding.visitanteMasUno.setOnClickListener {
+visitantePuntos+=1
+binding.visitantePuntos.text=visitantePuntos.toString()
+}
+
+binding.visitanteMasDos.setOnClickListener {
+visitantePuntos+=2
+binding.visitantePuntos.text=visitantePuntos.toString()
+}
+
+binding.btnSiguiente.setOnClickListener {
+val intent = Intent(this, FinalResultActivity::class.java)
+startActivity(intent)
+}
+}
+
+
+
+}
+
+
+
+ *
+ *
+ * */
